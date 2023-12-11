d2d9bea23113fddc4950f14ed5182382b097e62d
NebulerStar <153537222+Nebuler-Star@users.noreply.github.com>
2023-12-11 12:15:31 -0500

    Save

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 const canv = document.getElementById("game-canvas");
 const ctx = canv.getContext("2d");
 
+canv.height = window.innerHeight;
+canv.width = window.innerWidth;
+
+ctx.fillStyle="green";
+ctx.fillRect(200, 200, 200, 200)
