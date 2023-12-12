8048e5b5208e457d98bc148ec0493759dfa2e9b3
NebulerStar <153537222+Nebuler-Star@users.noreply.github.com>
2023-12-12 14:17:49 -0500

    Save

--- js/index.js
+++ js/index.js
@@ -4,5 +4,13 @@ const ctx = canv.getContext("2d");
 canv.height = window.innerHeight;
 canv.width = window.innerWidth;
 
-ctx.fillStyle="green";
-ctx.fillRect(200, 200, 200, 200)
+function draw(){
+    ctx.fillStyle="green";
+    ctx.fillRect(200, 200, 200, 200)
+}
+
+function animate() {
+    draw();
+    window.requestAnimationFrame(animate);
+}
+animate();
